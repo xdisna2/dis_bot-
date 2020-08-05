2a8ed3b62725183809af3a40cf4cd52bb7e328b9
Anthony Le <aznthonyle45@gmail.com>
2020-08-05 15:50:23 -0700

    Basic Code for a minimal bot

---
+++ test.py
@@ -0,0 +1,23 @@
+# Imports the Discord.py module
+import discord
+
+# This is our bot client's connection to Discord itself
+client = discord.Client()
+
+# Registers an event
+@client.event
+# This event is called when the bot logs in and sets things up
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+# When the bot RECEIVES a message
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await  message.channel.send('Hello!')
+
+# Runs the bot with our LOGIN Token
+client.run('NzMxNjcxNTUxMDk5MDExMDgy.XwpcOw.pc1oRaY0Qdy9CrhWTyrnN2WOAug')
