ff46dd24575f4a0ba3242282dbc1daf59ba4abd5
Anthony Le <aznthonyle45@gmail.com>
2020-08-06 17:31:29 -0700

    Added events for member join and remove

--- bot.py
+++ bot.py
@@ -8,10 +8,23 @@ client = commands.Bot(command_prefix = '$start')
 
 # Denotes an event
 @client.event
-
 # When the bot is in a ready normal state do this
 async def on_ready():
     print("Hello Everyone I am PersOna!")
 
+# Client bot has connected to discord
+@client.event
+async def on_connect():
+    print("Connected to Discord...")
+
+# Greets someone that has joined the server
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the testing server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has been BANISHED TO THE SHADOW REALM')
+
 # Runs the bot and please put in Bot TOKEN
-client.run('')
+client.run('NzMxNjcxNTUxMDk5MDExMDgy.XwpcOw.cvjQMQXq6rv4aeOC7H177CPTwGU')
