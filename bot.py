7ebd0e9ad208548b9069f2886f30e0e3a611d139
Anthony Le <aznthonyle45@gmail.com>
2020-08-09 14:04:03 -0700

    Added first command that greets and sends the latency of the bot

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 from discord.ext import commands
 
 # Bot command
-client = commands.Bot(command_prefix = '$start')
+client = commands.Bot(command_prefix = '$')
 
 # Denotes an event
 @client.event
@@ -22,9 +22,16 @@ async def on_connect():
 async def on_member_join(member):
     print(f'{member} has joined the testing server.')
 
+# Tells us that a user has been banned or left the server
 @client.event
 async def on_member_remove(member):
     print(f'{member} has been BANISHED TO THE SHADOW REALM')
 
+# Upon using $convo command, sends a message
+@client.command()
+async def convo(ctx):
+    await ctx.send('Hello there!')
+    await ctx.send(f'My Latency is {round(client.latency * 1000)}ms ')
+
 # Runs the bot and please put in Bot TOKEN
-client.run('NzMxNjcxNTUxMDk5MDExMDgy.XwpcOw.cvjQMQXq6rv4aeOC7H177CPTwGU')
+client.run('NzMxNjcxNTUxMDk5MDExMDgy.XwpcOw.mBvQatZBpVN4ZC56SawIobNfe5s')
